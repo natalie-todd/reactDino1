bf207e0370712d6d5b907e88ed628f7b6176b135
Natalie <natalie.todd15@gmail.com>
2018-07-17 10:34:33 -0600

    new repo for dino 1, react working, about to do api fetch

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+
+class Background extends Component {
+    constructor () {
+        super();
+        this.state = {
+            pictures: [],
+        };
+    }
+}
